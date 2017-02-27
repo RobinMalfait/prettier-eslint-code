4ebfd87ec16bb936f9b467add53c1111f57acdab
Robin Malfait <malfait.robin@gmail.com>
2017-02-27 19:52:50 +0100

    first attempt
    Now we just need to refactor and fine-tune

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,88 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
+const formatPrettierESLint = require('prettier-eslint');
+
+const allowedLanguages = ['javascript'];
+
+function fullDocumentRange(document: vscode.TextDocument): vscode.Range {
+    const lastLineId = document.lineCount - 1;
+    return new vscode.Range(0, 0, lastLineId, document.lineAt(lastLineId).text.length);
+}
+
+let statusbar = undefined;
+
+function showStatusBarMessage (message, tooltip) {
+    if (statusbar === undefined) {
+        statusbar = vscode.window.createStatusBarItem();
+    }
+
+    statusbar.text = message;
+    statusbar.tooltip = tooltip;
+    statusbar.show();
+}
+
+function format (textDocument: vscode.TextDocument) {
+    // Get information from the document
+    const language = textDocument.languageId;
+
+    // It is not an allowed language
+    if (allowedLanguages.indexOf(language) === -1) {
+        return;
+    }
+
+    const filePath = textDocument.fileName;
+    const text = textDocument.getText();
+
+    // Try to format the code
+    try {
+        let formattedOutput = formatPrettierESLint({ text, filePath });
+
+        // It is the same output now
+        if (formattedOutput === text) {
+            showStatusBarMessage(`Prettier ESLint: $(check)`, 'All good!');
+            return;
+        }
+
+        const documentRange = fullDocumentRange(textDocument);
+
+        const editor = vscode.window.activeTextEditor;
+        editor
+            .edit((editBuilder) => editor.edit((editBuilder) => editBuilder.replace(documentRange, formattedOutput)))
+            .then(() => textDocument.save())
+            .then(
+                () => showStatusBarMessage('Prettier ESLint: $(check)', 'All good!'),
+                (err) => showStatusBarMessage('Prettier ESLint: $(x)', err.toString())
+            );
+    } catch (err) {
+        showStatusBarMessage('Prettier ESLint: $(x)', err.toString());
+    }
+}
+
+const registerDisposable = (context) => (cb = () => {}) => {
+    context.subscriptions.push(cb());
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "prettier-eslint" is now active!');
+    // Create register function
+    const register = registerDisposable(context);
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    // Register change listener
+    register(() => {
+        return vscode.workspace.onDidSaveTextDocument((document) => {
+            format(document);
+        });
     });
 
-    context.subscriptions.push(disposable);
+    // Register manual format listener
+    register(() => {
+        return vscode.commands.registerCommand('prettier-eslint.format', () => {
+            format(vscode.window.activeTextEditor.document);
+        });
+    });
 }
 
 // this method is called when your extension is deactivated
